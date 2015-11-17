fbd2829e465be3abe34795458b3eed7676504d41
liuhangbiao <liuhangbiaoo@gmail.com>
2015-11-17 14:07:39 +0800

    修改 package.json 文件

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+    // 项目配置
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+            ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+        //开始
+        uglify: {
+            build: {
+                src: 'src/jquery-1.11.1.js',
+                dest: 'dest/j.min.js'
+            }
+        }
+        //结束
+    });
+    // 加载提供"uglify"任务的插件
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    // 默认任务
+    grunt.registerTask('default', ['uglify']);
+}
